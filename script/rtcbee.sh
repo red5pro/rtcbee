d539bc84107198c3fbbd5c2d20784a71ec295c7b
bustardcelly <bustardcelly@gmail.com>
2018-01-31 12:52:21 -0500

    better sleep offload for status checks in bash.

--- script/rtcbee.sh
+++ script/rtcbee.sh
@@ -4,39 +4,22 @@ DEBUG_PORT_START=9000
 endpoint=$1
 amount=$2
 timeout=$3
-debug_port=$DEBUG_PORT_START
 pids=()
 
 ulimit -n 65536
 ulimit -c unlimited
 sysctl fs.inotify.max_user_watches=13166604
 
+rm -rf log
 mkdir -p log
 
-dt=$(date '+%d/%m/%Y %H:%M:%S')
-echo "Starting attack, $dt"
-
-for ((i=1;i<=$amount;i++)); do
-  debug_port=$((debug_port + 1))
-  echo "Dispatching Bee $i..."
-#  chromium-browser --single-process --user-data-dir=/tmp/chrome$(date +%s%N) --headless --disable-gpu --mute-audio --window-size=1024,768 --remote-debugging-port=$debug_port $endpoint >&1 | grep CONSOLE > "logs/rtcbee_${debug_port}.log" &
-  /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --single-process --user-data-dir=/tmp/chrome$(date +%s%N) --headless --disable-gpu --mute-audio --window-size=1024,768 --remote-debugging-port=$debug_port $endpoint 3>&1 1>"log/rtcbee_${debug_port}.log" 2>&1 &
-  pid=$(echo $!)
-  pids[$i-1]=$pid
-  echo "PID filed as: ${pid}."
-  sleep 1
-done
-
-dt=$(date '+%d/%m/%Y %H:%M:%S');
-echo "Attack deployed, $dt"
-# echo $pids
-
-sleep 5
-debug_port=$DEBUG_PORT_START
-
-for ((i=1;i<=$amount;i++)); do
-  debug_port=$((debug_port + 1))
+function checkStatus {
+  bee=$1
+  debug_port=$((DEBUG_PORT_START + bee))
   log="log/rtcbee_${debug_port}.log"
+  pid=${pids[${bee}-1]}
+
+  echo "Check Status on Bee $bee..."
 
   # if out match /Stream \w+ does not exist/g -> error
   failure=0
@@ -44,7 +27,7 @@ for ((i=1;i<=$amount;i++)); do
   regex_fail='Subscribe\.InvalidName'
   regex_success='Subscribe\.Start'
 
-  while read line
+  while read -r line
   do
     if [[ $line =~ $regex_fail ]]; then
       failure=1
@@ -55,16 +38,49 @@ for ((i=1;i<=$amount;i++)); do
 
   if [ $failure -eq 1 ]; then
     echo "--- ALERT ---"
-    echo "Bee $i failed in its mission. View ${log} for more details."
+    echo "Bee $bee failed in its mission. View ${log} for more details."
+    kill -9 "$pid" || echo "Failure to kill ${pid}."
     echo "--- // OVER ---"
   elif [[ $success -eq 1 ]]; then
     echo "--- Success ---"
-    echo "Bee $i has begin attack..."
+    echo "Bee $bee has begun attack..."
+    (sleep "$timeout"; kill -9 "$pid" || echo "Failure to kill ${pid}.")&
     echo "--- // OVER ---"
+  else
+    echo "no report yet for $bee..."
+    (sleep 2; checkStatus "$bee")&
   fi
+}
 
-  pid=${pids[${i}-1]}
-  echo "Killing PID: ${pid}."
-  kill -9 $pid
+dt=$(date '+%d/%m/%Y %H:%M:%S')
+echo "Starting attack at $dt"
+
+for ((i=1;i<=amount;i++)); do
+  debug_port=$((DEBUG_PORT_START + i))
+  echo "Dispatching Bee $i..."
+#  chromium-browser --single-process --user-data-dir=/tmp/chrome$(date +%s%N) --headless --disable-gpu --mute-audio --window-size=1024,768 --remote-debugging-port=$debug_port $endpoint >&1 | grep CONSOLE > "logs/rtcbee_${debug_port}.log" &
+  /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --single-process --user-data-dir=/tmp/chrome"$(date +%s%N)" --headless --disable-gpu --mute-audio --window-size=1024,768 --remote-debugging-port=$debug_port "$endpoint" 3>&1 1>"log/rtcbee_${debug_port}.log" 2>&1 &
+  pid=$!
+  pids[$i-1]=$pid
+  echo "PID filed as: ${pid}."
+  sleep 1
+done
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "Attack deployed at $dt"
+
+if [[ $amount -lt 10 ]]; then
+  echo "Giving the bees some time to setup..."
+  sleep 5
+else
+  echo "Because we spaced the bees out, let's start checking on their status..."
+fi
+
+debug_port=$DEBUG_PORT_START
+
+echo "Checking in on our $amount bees..."
+
+for ((i=1;i<=amount;i++)); do
+  checkStatus $i
 done
 
