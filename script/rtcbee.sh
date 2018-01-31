f28d90db6b3bb5fb78510b98f6281dbf2aa99a03
bustardcelly <bustardcelly@gmail.com>
2018-01-31 09:57:08 -0500

    update to script

--- script/rtcbee.sh
+++ script/rtcbee.sh
@@ -18,13 +18,13 @@ echo "Starting attack, $dt"
 
 for ((i=1;i<=$amount;i++)); do
   debug_port=$((debug_port + 1))
-  echo "Sending out $i..."
+  echo "Dispatching Bee $i..."
 #  chromium-browser --single-process --user-data-dir=/tmp/chrome$(date +%s%N) --headless --disable-gpu --mute-audio --window-size=1024,768 --remote-debugging-port=$debug_port $endpoint >&1 | grep CONSOLE > "logs/rtcbee_${debug_port}.log" &
   /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --single-process --user-data-dir=/tmp/chrome$(date +%s%N) --headless --disable-gpu --mute-audio --window-size=1024,768 --remote-debugging-port=$debug_port $endpoint 3>&1 1>"log/rtcbee_${debug_port}.log" 2>&1 &
   pid=$(echo $!)
   pids[$i-1]=$pid
   echo "PID filed as: ${pid}."
-  sleep 3
+  sleep 1
 done
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
@@ -37,12 +37,34 @@ debug_port=$DEBUG_PORT_START
 for ((i=1;i<=$amount;i++)); do
   debug_port=$((debug_port + 1))
   log="log/rtcbee_${debug_port}.log"
-  out=$(cat $log)
+
   # if out match /Stream \w+ does not exist/g -> error
-  # if out match
-  match=($(grep -o 'Stream \w+ does not exist' $log))
-  echo $match
+  failure=0
+  success=0
+  regex_fail='Subscribe\.InvalidName'
+  regex_success='Subscribe\.Start'
+
+  while read line
+  do
+    if [[ $line =~ $regex_fail ]]; then
+      failure=1
+    elif [[ $line =~ $regex_success ]]; then
+      success=1
+    fi
+  done < $log
+
+  if [ $failure -eq 1 ]; then
+    echo "--- ALERT ---"
+    echo "Bee $i failed in its mission. View ${log} for more details."
+    echo "--- // OVER ---"
+  elif [[ $success -eq 1 ]]; then
+    echo "--- Success ---"
+    echo "Bee $i has begin attack..."
+    echo "--- // OVER ---"
+  fi
+
   pid=${pids[${i}-1]}
   echo "Killing PID: ${pid}."
   kill -9 $pid
 done
+
